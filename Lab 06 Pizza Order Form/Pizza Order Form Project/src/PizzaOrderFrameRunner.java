4223768831158dc6b85e82add1c33acda019866f
psolademi <114344232+psolademi@users.noreply.github.com>
2024-02-16 09:48:20 -0500

    Add files via upload

---
+++ Lab 06 Pizza Order Form/Pizza Order Form Project/src/PizzaOrderFrameRunner.java
@@ -0,0 +1,13 @@
+// Import statement for JFrame
+import javax.swing.JFrame;
+
+// Class definition for PizzaOrderFrameRunner
+public class PizzaOrderFrameRunner {
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        // Creating an instance of PizzaOrderFrame
+        JFrame frame = new PizzaOrderFrame();
+    }
+
+}
